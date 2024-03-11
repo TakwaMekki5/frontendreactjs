0922ec71b1b2e72fcff623a4f0da3dc26a931579
TakwaMekki5 <162512546+TakwaMekki5@users.noreply.github.com>
2024-03-11 21:56:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Login} from "./component/login";
-import {Home} from "./component/home";
-import {Navigation} from './component/Navigation';
-import {Logout} from './component/logout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from "./component/login";
+import { Home } from "./component/home";
+import { Navigation } from './component/Navigation';
+import { Logout } from './component/logout';
+
 function App() {
-    return <BrowserRouter>
-    <Navigation></Navigation>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/logout" element={<Logout/>}/>
-        </Routes>
-    </BrowserRouter>;
+    return (
+        <BrowserRouter>
+            <Navigation />
+            <div className="form-container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
